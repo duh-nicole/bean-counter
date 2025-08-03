340ef29d229af994e25facd6be46cf5651dd9086
Nicole Duhan <215088978+duh-nicole@users.noreply.github.com>
2025-08-02 21:53:37 -0500

    Update add_product_form.php

--- add_product_form.php
+++ add_product_form.php
@@ -15,7 +15,10 @@ $statement->closeCursor();
 
 <head>
     <title>CodeLatte's Studio Inventory</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="main.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -71,3 +74,4 @@ $statement->closeCursor();
     </footer>
 </body>
 </html>
+
